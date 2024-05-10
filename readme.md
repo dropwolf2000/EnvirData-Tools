25f878ac51f0c9a2a828a070bb44bd24887a18cb
LTZ26 <Ltz2639@hotmail.com>
2024-05-10 14:36:18 +0800

    创建项目，添加了dat转netcdf的功能

---
+++ readme.md
@@ -0,0 +1,12 @@
+# environment-convert-tools
+
+---
+
+## 用于实现各种水文学环境数据格式之间的转换工具, 顺便重构之前的各种转换工具
+
+### 已经实现的功能
+
+#### GSMaP 
+- [x] GSMaP -> NetCDF
+- [ ] GSMaP -> GeoTIFF
+
---
+++ src/gsmap/unpack_gz.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time       : 2023/6/8 19:57
+# @Author     : 落狼
+# @File       : batch_unpacking.py
+# @Environment: Python 3.9.1.2
+# @Software   : PyCharm
+
+import gzip
+import os
+
+
+def batch_extract_gz_files(input_dir, output_dir):
+    # 检查输出目录是否存在，若不存在则创建
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # 遍历输入目录下的所有文件
+    for root, dirs, files in os.walk(input_dir):
+        for filename in files:
+            # 检查文件扩展名是否为.gz
+            if filename.endswith('.gz'):
+                input_path = os.path.join(root, filename)
+                output_path = os.path.join(output_dir, os.path.splitext(filename)[0])
+
+                # 解压缩gz文件
+                with gzip.open(input_path, 'rb') as f_in, open(output_path, 'wb') as f_out:
+                    f_out.write(f_in.read())
+                    print(f'解压缩文件: {filename}')
+
+
+if __name__ == '__main__':
+
+    # 使用示例
+    input_dir = r'D:\DATA\gsmap_testdata\origin'  # 输入目录，存放gz文件的目录
+    output_dir = r'D:\DATA\gsmap_testdata\dat'  # 输出目录，存放解压缩后的文件的目录
+
+    batch_extract_gz_files(input_dir, output_dir)
